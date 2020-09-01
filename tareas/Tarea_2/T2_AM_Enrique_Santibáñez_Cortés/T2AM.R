2b88f78effc47dff97df1bc22d098aa3f6375533
Enriquesec <enriquesec@gmail.com>
2020-09-01 17:27:32 -0500

    Copie la Tarea 2.

---
+++ tareas/Tarea_2/T2_AM_Enrique_Santibáñez_Cortés/T2AM.R
@@ -0,0 +1,12 @@
+'1. Programe una función en r que reciba de entrada por parte del usuario el tamaño de una matriz y las entradas de la misma en orden de izquierda a derecha, 
+de arriba a abajo. Luego, debe ir preguntando que operación elemental debe hacerse, y una vez que el usuario indique precisamente cuál, diciendo exactamente
+que renglones y números estarán involucrados, debe mostrarle al usuario la matriz resultante.  Cuando el usuario llegue a una forma escalonada, deberá decir-
+le al usuario, mostrarle la matriz final y terminar el programa. Usted debe especificar al usuario cómo y en que orden debe introducir los valores y hacer 
+las validaciones correspondientes.
+
+2. Programe una función en r que reciba de entrada una matriz y que realice la eliminación de Gauss y regrese la matriz escalonada por renglones. Es decir, 
+a diferencia del problema 1 no debe haber intervención del usuario más que en la captura de la matriz.
+
+
+En ninguno de los dos programas se pueden usar comandos o librerías de r que resuelvan directamente el problema. Si puede usar librerías que mejoren la captu-
+ra de datos o la presentación del programa.'
