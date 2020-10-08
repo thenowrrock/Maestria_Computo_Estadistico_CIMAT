d9eb1da357a2c088788a1bbcbb15556a9803a4f5
Enriquesec <enriquesec@gmail.com>
2020-10-08 10:28:32 -0500

    Tarea 3 terminada.

---
+++ Tareas/Tarea_3/T3_P1_Enrique_Santibanez_Cortes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+#define N 5
+
+using namespace std;
+
+int* ApplyFunction(int *arr){
+	static int ArrOut[N]; // Memoria estatica.
+	for(int i=0; i<N; i++){
+		ArrOut[i] = arr[i]; // Copiamos todos los elementes.
+	}
+	return &ArrOut[0]; // retornamos la posicion
+}
+
+int main(){
+	int arr[N] = {1,2,6,0,5};
+	int *p = ApplyFunction(arr); // Usamos la funcion.
+	cout << *p << endl; // Imprimimos.
+	return 0;
+}
